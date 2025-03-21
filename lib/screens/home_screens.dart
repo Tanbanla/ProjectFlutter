510b2108aaa64061a5ff9e192e5a736abd577e60
AP\khanhmf <khanhmf@brothergroup.net>
2025-03-21 08:38:35 +0700

    four

--- lib/screens/home_screens.dart
+++ lib/screens/home_screens.dart
@@ -1,180 +1,3 @@
-// import 'dart:ui';
-
-// import 'package:flutter/material.dart';
-// import 'package:flutter/services.dart';
-
-// class HomeScreens extends StatelessWidget {
-//   const HomeScreens({super.key});
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       backgroundColor: Colors.black,
-//       extendBodyBehindAppBar:  true,
-//       appBar: AppBar(
-//         backgroundColor: Colors.transparent,
-//         elevation: 0,
-//         systemOverlayStyle: const SystemUiOverlayStyle(
-//           statusBarBrightness: Brightness.dark,
-//           ),
-//       ),
-//       body: Padding(
-//         padding: const EdgeInsets.fromLTRB(40, 1.2*kToolbarHeight, 40, 20),
-//         child: SizedBox(
-//           height: MediaQuery.of(context).size.height,
-//           child: Stack(
-//             children: [
-//               Align(
-//                 alignment: AlignmentDirectional(3, -0.3),
-//                 child: Container(
-//                   height: 300,
-//                   width:  300,
-//                   decoration: BoxDecoration(
-//                     shape: BoxShape.circle,
-//                     color: Colors.deepPurple
-//                   ),
-//                 ),
-//               ),
-//               Align(
-//                 alignment: AlignmentDirectional(-3, -0.3),
-//                 child: Container(
-//                   height: 300,
-//                   width:  300,
-//                   decoration: BoxDecoration(
-//                     shape: BoxShape.circle,
-//                     color: Colors.deepPurple
-//                   ),
-//                 ),
-//               ),
-//               Align(
-//                 alignment: AlignmentDirectional(0, -1.2),
-//                 child: Container(
-//                   height: 300,
-//                   width:  600,
-//                   decoration: BoxDecoration(
-//                     color: const Color.fromARGB(255, 241, 134, 85)
-//                   ),
-//                 ),
-//               ),
-//               BackdropFilter(
-//                 filter: ImageFilter.blur(sigmaX: 100, sigmaY:  100),
-//                 child: Container(
-//                   decoration: const BoxDecoration(color:  Colors.transparent),
-//                 ),
-//               ),
-//               SizedBox(
-//                 width: MediaQuery.of(context).size.width,
-//                 height: MediaQuery.of(context).size.height,
-//                 child: Column(
-//                   crossAxisAlignment: CrossAxisAlignment.start,
-//                   children: [
-//                     Text('😶 Hải Dương', style: TextStyle( color: Colors.white, fontWeight: FontWeight.w300, fontSize: 16 ),),
-//                     SizedBox(height: 8,),
-//                     Text('Good Moring', style: TextStyle( color: Colors.white, fontWeight: FontWeight.bold, fontSize: 32 ),),
-//                     Center(
-//                       child: Image.asset(
-//                         'assets/1.png',
-//                         height: 320,
-//                       ),
-//                     ),
-//                     const Center(
-//                       child: Text('21 C', style: TextStyle( color: Colors.white, fontWeight: FontWeight.w600, fontSize: 55 ),),
-//                     ),
-//                     const Center(
-//                       child: Text('THUNDERSTORM', style: TextStyle( color: Colors.white, fontWeight: FontWeight.w500, fontSize: 25 ),),
-//                     ),
-//                     const SizedBox(height: 5,),
-//                     const Center(
-//                       child: Text('Friday 16 ~ 09:41am', style: TextStyle( color: Colors.white, fontWeight: FontWeight.w300, fontSize: 16 ),),
-//                     ),
-//                     const SizedBox(height: 30,),
-//                     Row(
-//                       mainAxisAlignment: MainAxisAlignment.center,
-//                       children: [
-//                         Row(
-//                           children: [
-//                             Image.asset('assets/11.png', scale: 8,),
-//                             const SizedBox(width:  5,),
-//                             const Column(
-//                               crossAxisAlignment: CrossAxisAlignment.start,
-//                               children: [
-//                                 Text('Sunrise', style: TextStyle( color: Colors.white, fontWeight: FontWeight.w300,)),
-//                                 SizedBox(height: 3,),
-//                                 Text('05:31 am', style: TextStyle( color: Colors.white, fontWeight: FontWeight.w700)),
-//                               ],
-//                             ),
-
-//                           ],
-//                         ),
-//                         SizedBox(width: 32,),
-//                         Row(
-//                           children: [
-//                             Image.asset('assets/12.png', scale: 8,),
-//                             const SizedBox(width:  5,),
-//                             const Column(
-//                               crossAxisAlignment: CrossAxisAlignment.start,
-//                               children: [
-//                                 Text('Sunset', style: TextStyle( color: Colors.white, fontWeight: FontWeight.w300,)),
-//                                 SizedBox(height: 3,),
-//                                 Text('09:41 am', style: TextStyle( color: Colors.white, fontWeight: FontWeight.w700)),
-//                               ],
-//                             ),
-
-//                           ],
-//                         ),
-//                       ],
-//                     ),
-//                     const Padding(
-//                       padding: EdgeInsets.symmetric(vertical: 5),
-//                       child: Divider(
-//                         color: Colors.grey,
-//                       ),
-//                     ),
-//                     Row(
-//                       mainAxisAlignment: MainAxisAlignment.center,
-//                       children: [
-//                         Row(
-//                           children: [
-//                             Image.asset('assets/13.png', scale: 8,),
-//                             const SizedBox(width:  5,),
-//                             const Column(
-//                               crossAxisAlignment: CrossAxisAlignment.start,
-//                               children: [
-//                                 Text('Temp Max', style: TextStyle( color: Colors.white, fontWeight: FontWeight.w300,)),
-//                                 SizedBox(height: 3,),
-//                                 Text('12*C', style: TextStyle( color: Colors.white, fontWeight: FontWeight.w700)),
-//                               ],
-//                             ),
-
-//                           ],
-//                         ),
-//                         SizedBox(width: 32,),
-//                         Row(
-//                           children: [
-//                             Image.asset('assets/14.png', scale: 8,),
-//                             const SizedBox(width:  5,),
-//                             const Column(
-//                               crossAxisAlignment: CrossAxisAlignment.start,
-//                               children: [
-//                                 Text('Temp Min', style: TextStyle( color: Colors.white, fontWeight: FontWeight.w300,)),
-//                                 SizedBox(height: 3,),
-//                                 Text('8*C', style: TextStyle( color: Colors.white, fontWeight: FontWeight.w700)),
-//                               ],
-//                             ),
-
-//                           ],
-//                         ),
-//                       ],
-//                     ),
-//                   ],
-//                 ),
-//               ),
-//             ],
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
@@ -253,7 +76,6 @@ class _HomeScreenState extends State<HomeScreen> {
             );
 		}
 	}
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -320,10 +142,11 @@ class _HomeScreenState extends State<HomeScreen> {
 												crossAxisAlignment: CrossAxisAlignment.start,
 												children: [
 													Text(
-														'📍 ${state.weather.areaName}',
+														'🎅🎉 ${state.weather.areaName}',
 														style: const TextStyle(
 															color: Colors.white,
-															fontWeight: FontWeight.w300
+															fontWeight: FontWeight.w300,
+                              fontSize: 20,
 														),
 													),
 													const SizedBox(height: 8),
@@ -331,7 +154,7 @@ class _HomeScreenState extends State<HomeScreen> {
 														'Good Morning',
 														style: TextStyle(
 															color: Colors.white,
-															fontSize: 25,
+															fontSize: 32,
 															fontWeight: FontWeight.bold
 														),
 													),
